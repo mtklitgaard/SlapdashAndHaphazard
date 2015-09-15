8466991ef83bd9dfc5256b43eae564172f23e5dd
Dr. Klitty <mklittgaard@1800contacts.com>
2015-09-14 20:27:15 -0600

    add unity and first page and basic layout

--- SlapdashAndHaphazard/SlapdashAndHaphazard/Global.asax.cs
+++ SlapdashAndHaphazard/SlapdashAndHaphazard/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SlapdashAndHaphazard.App_Start;
 
 namespace SlapdashAndHaphazard
 {
@@ -13,6 +14,8 @@ namespace SlapdashAndHaphazard
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            
+            UnityWebActivator.Start();
         }
     }
 }
---
+++ SlapdashAndHaphazard/SlapdashAndHaphazard/Models/ListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SlapdashAndHaphazard.Domain;
+
+namespace SlapdashAndHaphazard.Models
+{
+    public class ListViewModel
+    {
+        public IList<Post> Posts { get; set; }
+        public int TotalPosts { get; set; }
+    }
+}
